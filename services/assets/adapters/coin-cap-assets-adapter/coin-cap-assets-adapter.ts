b9675ada6633e84a700cfacb850f68b8c0f015fa
ACER\danie <danielalarconcuesta@gmail.com>
2025-12-07 15:11:07 -0600

    #5mjbf7136: Update CoinCap API version to v3

--- services/assets/adapters/coin-cap-assets-adapter/coin-cap-assets-adapter.ts
+++ services/assets/adapters/coin-cap-assets-adapter/coin-cap-assets-adapter.ts
@@ -1,70 +1,83 @@
 import { Asset, GetAssetsResponse } from '@common-types/assets';
 import { parseAssetIconId, prepareCoinCapAssetsUrl } from '@services/assets/adapters/coin-cap-assets-adapter/helpers';
 import { GetCoinCapAssetResponse, GetCoinCapAssetsPayload } from '@services/assets/adapters/coin-cap-assets-adapter/types';
-import { urlErrorResponse, defaultErrorResponse } from '@services/assets/utils';
+import { urlErrorResponse, forbiddenErrorResponse, defaultErrorResponse } from '@services/assets/utils';
 
-export const COIN_CAP_ASSETS_ENDPOINT = 'https://api.coincap.io/v2/assets';
+export const COIN_CAP_ASSETS_ENDPOINT = 'https://rest.coincap.io/v3/assets';
 const MAX_AMOUNT_ASSETS = 2296;
 
+const token = process.env.COIN_CAP_API_KEY;
+
 const getAssets = async ({ search, ids, limit, offset }: GetCoinCapAssetsPayload): Promise<GetAssetsResponse> => {
-    try {
-        const url = prepareCoinCapAssetsUrl({ search, ids, limit, offset });
+	try {
 
-        if (!url) {
-            return urlErrorResponse;
-        }
-		
-        const response = await fetch(url, { method: 'GET' });
+		if (!token) {
+			return forbiddenErrorResponse;
+		}
 
-        if (!response.ok) {
-            return {
-                data: [],
-                timestamp: Date.now(),
-                error: { 
-                    errorMessage: response.statusText, 
-                    httpError: response.status 
-                }
-            };
-        }
+		const url = prepareCoinCapAssetsUrl({ search, ids, limit, offset });
 
-        const coinCapData: GetCoinCapAssetResponse = await response.json();
+		if (!url) {
+			return urlErrorResponse;
+		}
 
-        const assets: Asset[] = coinCapData.data.map((asset) => ({
-            ...asset,
-            rank: Number(asset.rank),
-            supply: Number(asset.supply),
-            maxSupply: (asset.maxSupply && asset.maxSupply !== 'null') ? Number(asset.maxSupply) : null,
-            marketCapUsd: Number(asset.marketCapUsd),
-            volumeUsd24Hr: Number(asset.volumeUsd24Hr),
-            priceUsd: Number(asset.priceUsd),
-            changePercent24Hr: Number(asset.changePercent24Hr),
-            vwap24Hr: Number(asset.vwap24Hr)
-        }));
+		const response = await fetch(url, {
+			headers: {
+				"Authorization": `Bearer ${token}`,
+				"Accept": "application/json"
+			},
+			method: 'GET'
+		});
 
-        const data: GetAssetsResponse = {
-            ...coinCapData,
-            data: assets
-        };
+		if (!response.ok) {
+			return {
+				data: [],
+				timestamp: Date.now(),
+				error: {
+					errorMessage: response.statusText,
+					httpError: response.status
+				}
+			};
+		}
 
-        return data;
+		const coinCapData: GetCoinCapAssetResponse = await response.json();
 
-    } catch(error) {
-        return defaultErrorResponse;
-    }
+		const assets: Asset[] = coinCapData.data.map((asset) => ({
+			...asset,
+			rank: Number(asset.rank),
+			supply: Number(asset.supply),
+			maxSupply: (asset.maxSupply && asset.maxSupply !== 'null') ? Number(asset.maxSupply) : null,
+			marketCapUsd: Number(asset.marketCapUsd),
+			volumeUsd24Hr: Number(asset.volumeUsd24Hr),
+			priceUsd: Number(asset.priceUsd),
+			changePercent24Hr: Number(asset.changePercent24Hr),
+			vwap24Hr: Number(asset.vwap24Hr)
+		}));
+
+		const data: GetAssetsResponse = {
+			...coinCapData,
+			data: assets
+		};
+
+		return data;
+
+	} catch (error) {
+		return defaultErrorResponse;
+	}
 };
 
-export const getCoinCapAssetIconUrl = async (symbol: string)  => await new Promise<string>((resolve) => {
-    resolve(`https://assets.coincap.io/assets/icons/${parseAssetIconId(symbol)}@2x.png`);
+export const getCoinCapAssetIconUrl = async (symbol: string) => await new Promise<string>((resolve) => {
+	resolve(`https://assets.coincap.io/assets/icons/${parseAssetIconId(symbol)}@2x.png`);
 });
 
-export const getMaxAmountAssets = async ()  => await new Promise<number>((resolve) => {
-    resolve(MAX_AMOUNT_ASSETS);
+export const getMaxAmountAssets = async () => await new Promise<number>((resolve) => {
+	resolve(MAX_AMOUNT_ASSETS);
 });
 
 const CoinCapAssetsAdapter = {
-    getAssets,
-    getCoinCapAssetIconUrl,
-    getMaxAmountAssets
+	getAssets,
+	getCoinCapAssetIconUrl,
+	getMaxAmountAssets
 };
 
 export default CoinCapAssetsAdapter;
