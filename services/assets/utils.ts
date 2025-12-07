1bd336f9e6e2776f5d04e71c3de0c990dc07272c
Daniel Alarcon <39710042+danielalarcondev@users.noreply.github.com>
2025-12-07 15:16:50 -0600

    #5mjbf7136: Update CoinCap API version to v3 (#23)

--- services/assets/utils.ts
+++ services/assets/utils.ts
@@ -7,21 +7,33 @@ export const DEFAULT_ERROR_CODE = 500;
 export const DEFAULT_ERROR_MESSAGE = 'Something went wrong';
 export const URL_ERROR_CODE = 404;
 export const URL_ERROR_MESSAGE = 'Server could not be reached. Try again later';
+export const FORBIDDEN_ERROR_CODE = 403;
+export const FORBIDDEN_ERROR_MESSAGE = 'Access forbidden. You do not have permission to access this resource.';
 
 export const defaultErrorResponse: GetAssetsResponse = {
-    data: [],
-    timestamp: Date.now(),
-    error: { 
-        errorMessage: DEFAULT_ERROR_MESSAGE, 
-        httpError: DEFAULT_ERROR_CODE
-    }
+	data: [],
+	timestamp: Date.now(),
+	error: {
+		errorMessage: DEFAULT_ERROR_MESSAGE,
+		httpError: DEFAULT_ERROR_CODE
+	}
 };
 
 export const urlErrorResponse: GetAssetsResponse = {
-    data: [],
-    timestamp: Date.now(),
-    error: { 
-        errorMessage: URL_ERROR_MESSAGE,
-        httpError: URL_ERROR_CODE
-    }
+	data: [],
+	timestamp: Date.now(),
+	error: {
+		errorMessage: URL_ERROR_MESSAGE,
+		httpError: URL_ERROR_CODE
+	}
 };
+
+export const forbiddenErrorResponse: GetAssetsResponse = {
+	data: [],
+	timestamp: Date.now(),
+	error: {
+		errorMessage: FORBIDDEN_ERROR_MESSAGE,
+		httpError: FORBIDDEN_ERROR_CODE
+	}
+};
+
